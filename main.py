d476903cd9622ddc4b2813b9f10a820f74d000e0
Lucas Straub <lucas.zimmer.straub@gmail.com>
2024-04-03 11:51:37 -0300

    filter project done, things to do with it still missing

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+video = cv.VideoCapture('cars.mp4')
+subtractor = cv.createBackgroundSubtractorMOG2(200,50)    #Identifica o objeto que se move, muito ruido.
+
+while True:
+    ret, frame = video.read()
+    
+    if ret:
+        #Visualize it
+        mask = subtractor.apply(frame)
+        cv.imshow("mask", mask)
+        
+        if cv.waitKey(5) == ord('X'):#BREAKS THE CODE
+            break
+    else:
+        #Creates a loop
+        video = cv.VideoCapture('cars.mp4')
+        
+cv.destroyAllWindows()
+video.release()
